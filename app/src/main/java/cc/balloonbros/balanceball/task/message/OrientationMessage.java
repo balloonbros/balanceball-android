814fcc6e68705f2cccc33af9dc894f82a405fbcc
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-09-14 06:19:15 +0900

    タイポ修正

--- app/src/main/java/cc/balloonbros/balanceball/task/message/OrientationMessage.java
+++ app/src/main/java/cc/balloonbros/balanceball/task/message/OrientationMessage.java
@@ -3,26 +3,26 @@ package cc.balloonbros.balanceball.task.message;
 import cc.balloonbros.balanceball.lib.task.message.AbstractTaskMessage;
 
 public class OrientationMessage extends AbstractTaskMessage {
-    private float[] mOrientatin =  new float[3];
+    private float[] mOrientation =  new float[3];
 
     public OrientationMessage(float[] orientations) {
         super("orientation");
-        mOrientatin = orientations;
+        mOrientation = orientations;
     }
 
     public float getAxisZ() {
-        return mOrientatin[0];
+        return mOrientation[0];
     }
 
     public float getAxisX() {
-        return mOrientatin[1];
+        return mOrientation[1];
     }
 
     public float getAxisY() {
-        return mOrientatin[2];
+        return mOrientation[2];
     }
 
     public float[] getOrientation() {
-       return mOrientatin;
+       return mOrientation;
     }
 }
