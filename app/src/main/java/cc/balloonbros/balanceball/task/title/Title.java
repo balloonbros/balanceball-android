749ea67d6e96c1aff7a75c37fe26c0da5a67e8de
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-09-21 08:00:11 +0900

    タイトル文字を中央に描画してみる

--- app/src/main/java/cc/balloonbros/balanceball/task/title/Title.java
+++ app/src/main/java/cc/balloonbros/balanceball/task/title/Title.java
@@ -3,6 +3,7 @@ package cc.balloonbros.balanceball.task.title;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 
 import cc.balloonbros.balanceball.lib.task.DrawableTask;
 
@@ -10,20 +11,31 @@ import cc.balloonbros.balanceball.lib.task.DrawableTask;
  * タイトルタスク
  */
 public class Title extends DrawableTask {
+    static private final String TITLE = "Balanceball";
+
     private Paint mPaint = new Paint();
+    private Point mTextPosition = new Point();
 
     @Override
     public void onRegistered() {
         mPaint.setColor(Color.GRAY);
+        mPaint.setTextSize(36.0F);
+        mPaint.setTextAlign(Paint.Align.CENTER);
+
+        Paint.FontMetrics fm = mPaint.getFontMetrics();
+        Point displaySize = getDisplaySize();
+
+        float fontHeight = fm.descent - fm.ascent;
+        float x = displaySize.x / 2;
+        float y = (displaySize.y / 2) + (fontHeight / 2) - fm.descent;
+        mTextPosition.set((int)x, (int)y);
     }
+
+    @Override
+    public void onUpdate() {  }
 
     @Override
     public void onDraw(Canvas canvas) {
-        canvas.drawText("Balanceball", 10, 10, mPaint);
-    }
-
-    @Override
-    public void onUpdate() {
-
+        canvas.drawText(TITLE, mTextPosition.x, mTextPosition.y, mPaint);
     }
 }
