3685a26a9a8de85b528bca75496ea19eb84e92d0
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-09-20 20:48:56 +0900

    ロゴをタッチすると次の画面へ進むように

--- app/src/main/java/cc/balloonbros/balanceball/task/launch/Logo.java
+++ app/src/main/java/cc/balloonbros/balanceball/task/launch/Logo.java
@@ -3,9 +3,13 @@ package cc.balloonbros.balanceball.task.launch;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 
 import cc.balloonbros.balanceball.R;
+import cc.balloonbros.balanceball.lib.task.Drawable;
 import cc.balloonbros.balanceball.lib.task.DrawableTask;
+import cc.balloonbros.balanceball.lib.task.basic.TouchTask;
+import cc.balloonbros.balanceball.lib.task.timer.Timer;
 import cc.balloonbros.balanceball.lib.task.timer.TimerEventListener;
 import cc.balloonbros.balanceball.scene.PlayScene;
 import cc.balloonbros.balanceball.scene.TitleScene;
@@ -13,22 +17,31 @@ import cc.balloonbros.balanceball.scene.TitleScene;
 /**
  * ロゴ表示タスク
  */
-public class Logo extends DrawableTask implements TimerEventListener {
+public class Logo extends TouchTask implements Drawable, TimerEventListener {
+    /** ロゴ画像の転送元矩形 */
     private Rect mSource = new Rect();
+
+    /** ロゴ画像の転送先矩形 */
     private Rect mDestination = new Rect();
+
+    /** ロゴ画像 */
     private Bitmap mLogo = null;
+
+    /** タイマー */
+    private Timer mTimer = null;
 
     @Override
     public void onRegistered() {
+        super.onRegistered();
+
         mLogo = getImage(R.drawable.launch_logo);
         mSource.set(0, 0, mLogo.getWidth(), mLogo.getHeight());
         mDestination.set(0, 0, getDisplaySize().x, getDisplaySize().y);
-        setTimer(5000, this);
+        mTimer = setTimer(5000, this);
     }
 
     @Override
-    public void onUpdate() {
-    }
+    public void onUpdate() { }
 
     @Override
     public void onDraw(Canvas canvas) {
@@ -38,5 +51,12 @@ public class Logo extends DrawableTask implements TimerEventListener {
     @Override
     public void onTimer() {
         changeScene(new TitleScene(getGame()));
+    }
+
+    @Override
+    public boolean onTouch(MotionEvent event) {
+        mTimer.stop();
+        onTimer();
+        return true;
     }
 }
