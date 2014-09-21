0b891424b0aa79d9c439da34f4fe082f55b7a1ac
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-09-21 10:48:06 +0900

    フォントのディレクトリを移動

--- app/src/main/java/cc/balloonbros/balanceball/task/title/Title.java
+++ app/src/main/java/cc/balloonbros/balanceball/task/title/Title.java
@@ -4,30 +4,31 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Typeface;
 
+import cc.balloonbros.balanceball.R;
 import cc.balloonbros.balanceball.lib.task.DrawableTask;
 
 /**
  * タイトルタスク
  */
 public class Title extends DrawableTask {
-    static private final String TITLE = "Balanceball";
-
     private Paint mPaint = new Paint();
     private Point mTextPosition = new Point();
 
     @Override
     public void onRegistered() {
-        mPaint.setColor(Color.GRAY);
-        mPaint.setTextSize(36.0F);
+        Typeface typeface = Typeface.createFromAsset(getGame().getContext().getAssets(), "fonts/opensans-bold.ttf");
+
+        mPaint.setColor(Color.rgb(0xfc, 0x50, 0x41));
+        mPaint.setTextSize(66.0F);
         mPaint.setTextAlign(Paint.Align.CENTER);
+        mPaint.setAntiAlias(true);
+        mPaint.setTypeface(typeface);
 
-        Paint.FontMetrics fm = mPaint.getFontMetrics();
         Point displaySize = getDisplaySize();
-
-        float fontHeight = fm.descent - fm.ascent;
         float x = displaySize.x / 2;
-        float y = (displaySize.y / 2) + (fontHeight / 2) - fm.descent;
+        float y = displaySize.y / 3;
         mTextPosition.set((int)x, (int)y);
     }
 
@@ -36,6 +37,6 @@ public class Title extends DrawableTask {
 
     @Override
     public void onDraw(Canvas canvas) {
-        canvas.drawText(TITLE, mTextPosition.x, mTextPosition.y, mPaint);
+        canvas.drawText(getString(R.string.app_name), mTextPosition.x, mTextPosition.y, mPaint);
     }
 }
