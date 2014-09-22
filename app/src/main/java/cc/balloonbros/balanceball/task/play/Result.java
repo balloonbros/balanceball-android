c99c9ee67c5fec55050e5f80f7262a2b74f60b37
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2014-09-23 07:36:09 +0900

    制限時間が終わったら結果タスクを追加

--- app/src/main/java/cc/balloonbros/balanceball/task/play/Result.java
+++ app/src/main/java/cc/balloonbros/balanceball/task/play/Result.java
@@ -1,6 +1,22 @@
 package cc.balloonbros.balanceball.task.play;
 
-import cc.balloonbros.balanceball.lib.task.AbstractTask;
+import android.graphics.Canvas;
+import android.graphics.Color;
 
-public class Result extends AbstractTask {
+import cc.balloonbros.balanceball.R;
+import cc.balloonbros.balanceball.lib._;
+import cc.balloonbros.balanceball.lib.task.AbstractTask;
+import cc.balloonbros.balanceball.lib.task.Drawable;
+
+public class Result extends AbstractTask implements Drawable {
+    @Override
+    public void onRegistered() {
+        super.onRegistered();
+        setPriority(_.i(R.integer.priority_result));
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLACK & 0xaa << 24);
+    }
 }
